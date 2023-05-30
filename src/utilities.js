194f3d8961007bc179046f5abbc8d84a0d4f6266
MariusValean <94649050+MariusValean@users.noreply.github.com>
2023-05-30 11:37:24 +0300

    Ultima versiune

---
+++ src/utilities.js
@@ -0,0 +1,26 @@
+export function sleep(ms) {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve();
+		}, ms);
+	});
+}
+
+export function $(selector) {
+	return document.querySelector(selector);
+}
+
+export function debounce(fn, ms) {
+	let timer;
+	return function () {
+		var context = this;
+		var args = arguments;
+		clearTimeout(timer);
+		timer = setTimeout(function () {
+			//fn(e);
+			//fn.call(this, e);
+			//fn.apply(this, arguments);
+			fn.apply(context, args);
+		}, ms);
+	};
+}
